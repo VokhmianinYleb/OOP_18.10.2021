4315b053bf761445c6e0958f62f359858a0902cf
Yleb22 <80745115+Yleb22@users.noreply.github.com>
2021-10-18 12:09:35 +0300

    Create main.cpp

---
+++ Example_10.3/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "CMyPoint.h"
+using namespace std;
+
+CMyPoint GetCenterPoint(CMyPoint mp)
+{
+	int tx, ty;
+	tx = mp.GetX() / 2;
+	ty = mp.GetY() / 2;
+	// повернення з функції, конструктор копіювання не викликається
+	// замість нього викликається конструктор з 2 параметрами
+	return CMyPoint(tx, ty);
+}
+// функція, що повертає середину відрізка, заданого точками
+CMyPoint GetCenterPoint2(int x, int y)
+{
+	CMyPoint mp(x / 2, y / 2);
+	// створюється тимчасовий об'єкт, який ініціалізується значенням з mp,
+	// у результаті викликається конструктор копіювання
+	return mp;
+	// у цьому випадку конструктор копіювання не викликається
+	//return CMyPoint(x/2,y/2);
+}
+
+//CMyPoint Distance(CMyPoint mp)
+//{
+//	int tx, ty;
+//	tx = mp.GetX(); ДОПИСАТЬ икс первой точки плюс икс второй поделить на 2
+//	ty = mp.GetY();
+//	return CMyPoint(tx, ty);
+//}
+
+int main() {
+	CMyPoint mp(18, -8);
+	CMyPoint mpC;
+	// викликається конструктор копіювання при передачі mp в функцію
+	mpC = GetCenterPoint(mp);
+	int cx, cy;
+	cx = mpC.GetX(); // cx = 9
+	cy = mpC.GetY(); // cy = -4
+	cout << cx << " " << cy << endl;
+	// викликається конструктор копіювання при поверненні з функції
+	mpC = GetCenterPoint2(-9, 13);
+	cx = mpC.GetX(); // cx = -4
+	cy = mpC.GetY(); // cy = 6
+	cout << cx << " " << cy << endl;
+	system("pause");
+}
